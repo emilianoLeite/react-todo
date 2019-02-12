8d5ff9a971f6b556ccd2266ca8a03966be95d7b1
Emiliano Leite <emiliano.leite@codus.com.br>
2019-02-12 17:19:03 -0200

    Allow user to create and list TODOs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import TodoInput from './components/todo-input';
+import TodoList from './components/todo-list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todos: this.getTodos() };
+  }
+
+  getTodos = () => {
+    let todos = JSON.parse(sessionStorage.getItem('todos-list'));
+    return (todos !== null) ? todos : [];
+  };
+
+  setTodos = (todo) => {
+    let todos = this.state.todos.concat(todo);
+    this.setState({ todos });
+    sessionStorage.setItem('todos-list', JSON.stringify(todos));
+  };
+
   render() {
     return (
       <div className="App">
+        <TodoInput onCreateTodo={this.setTodos}/>
+        <TodoList todos={this.state.todos}/>
       </div>
     );
   }
