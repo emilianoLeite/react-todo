c16ac0598f0929ebe5042088a7f0d33a4bd9ccc6
Emiliano Leite <emiliano.leite@codus.com.br>
2019-02-13 13:54:35 -0200

    Transform TodoInput into a ControlledComponent

--- src/components/todo-input.js
+++ src/components/todo-input.js
@@ -4,16 +4,31 @@ import PropTypes from "prop-types";
 export default class TodoInput extends React.Component {
   ENTER_KEY = 13;
 
-  render() {return (
-    <input onKeyPress={this.handleKeyPress} placeholder="New todo..."/>
-  );}
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+  }
 
   handleKeyPress = (event) => {
     let pressedKey = (event.keyCode ? event.keyCode : event.which);
+
     if (pressedKey === this.ENTER_KEY) {
       this.props.onCreateTodo(event.target.value);
     }
   };
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  };
+
+  render() { return (
+    <input
+      type="text"
+      placeholder="New todo..."
+      value={this.state.value}
+      onKeyPress={this.handleKeyPress}
+      onChange={this.handleChange}
+    />
+  );}
 }
 
 TodoInput.propTypes = {
