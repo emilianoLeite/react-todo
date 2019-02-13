1d8b99075342e18463cc83d7c5f2e7b5b4eddcb7
Emiliano Leite <leiteemiliano@gmail.com>
2019-02-12 22:46:35 -0200

    Add additional test case to TodoList

--- src/components/todo-list.test.js
+++ src/components/todo-list.test.js
@@ -5,3 +5,12 @@ import TodoList from "./todo-list";
 it("renders without crashing", () => {
   shallow(<TodoList />);
 });
+
+describe("when no todos are supplied", () => {
+  it("renders a empty list", () => {
+    let wrapper = shallow(<TodoList />);
+
+    expect(wrapper.find('ul').children()).toHaveLength(0);
+  });
+});
+
