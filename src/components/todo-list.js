84b8b4737de7f48b668291507893e96afeccc922
Emiliano Leite <emiliano.leite@codus.com.br>
2019-02-14 16:53:01 -0200

    Add filter menu to TodoList

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,3 +1,4 @@
+import "./todo-list.css";
 import PropTypes from "prop-types";
 import React from "react";
 
@@ -6,7 +7,15 @@ import Todo from "./todo";
 export default class TodoList extends React.Component {
   constructor(props) {
     super(props);
+    this.ALL = "All";
+    this.INCOMPLETE = "Incomplete";
+    this.COMPLETED = "Completed";
+    this.state = { activeMenu: this.ALL, visibleTodos: props.todos };
+
     this.handleClick = this.handleClick.bind(this);
+    this.isActiveMenu = this.isActiveMenu.bind(this);
+    this.activateMenu = this.activateMenu.bind(this);
+    this.visibleTodosFor = this.visibleTodosFor.bind(this);
   }
 
   handleClick(index) {
@@ -17,26 +26,70 @@ export default class TodoList extends React.Component {
     return (todo) => this.props.onUpdateTodo(todo, index);
   }
 
+  classesFor(menuItem) {
+    if (this.isActiveMenu(menuItem)) {
+      return "menu-item active";
+    } else {
+      return "menu-item";
+    }
+  }
+
+  isActiveMenu(menuItem) {
+    return this.state.activeMenu === menuItem;
+  }
+
+  activateMenu(event) {
+    const activeMenu = event.target.textContent.trim();
+    this.setState({
+      activeMenu: activeMenu,
+      visibleTodos: this.visibleTodosFor(activeMenu)
+    });
+  }
+
+  visibleTodosFor(activeMenu) {
+    const todos = this.props.todos;
+
+    switch (activeMenu) {
+    case this.ALL:
+      return todos;
+    case this.INCOMPLETE:
+      return todos;
+    case this.COMPLETED:
+      return todos;
+    default:
+      return [];
+    }
+  }
+
   render() {
+    const visibleTodos = this.visibleTodosFor(this.state.activeMenu);
+
     return (
-      <ul className="todo-list">
-        {
-          this.props.todos.map((todo, index) => {
-            return (
-              <li
-                className={todo.completed ? "completed" : ""}
-                key={index}
-              >
-                <input type="checkbox" onClick={() => this.handleClick(index)} />
-                <Todo
-                  onUpdateTodo={this.indexedHandleUpdateTodo(index)}
-                  todo={todo}
-                />
-              </li>
-            );
-          })
-        }
-      </ul>
+      <div className="todo-list">
+        <div className="filter-menu">
+          <div onClick={this.activateMenu} className={this.classesFor(this.ALL)}> {this.ALL} </div>
+          <div onClick={this.activateMenu} className={this.classesFor(this.INCOMPLETE)}> {this.INCOMPLETE} </div>
+          <div onClick={this.activateMenu} className={this.classesFor(this.COMPLETED)}> {this.COMPLETED} </div>
+        </div>
+        <ul>
+          {
+            visibleTodos.map((todo, index) => {
+              return (
+                <li
+                  className={todo.completed ? "completed" : ""}
+                  key={index}
+                >
+                  <input type="checkbox" onClick={() => this.handleClick(index)} />
+                  <Todo
+                    onUpdateTodo={this.indexedHandleUpdateTodo(index)}
+                    todo={todo}
+                  />
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
     );
   }
 }
