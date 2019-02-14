ed2b8c29f54482a3b4631836024b9c0bb0b3bd55
Emiliano Leite <emiliano.leite@codus.com.br>
2019-02-14 16:51:20 -0200

    Rename 'App' css class

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ export default class App extends Component {
   }
 
   render() { return (
-    <div className="App">
+    <div className="app">
       <TodoInput onCreateTodo={this.setTodo}/>
       <TodoList
         onUpdateTodo={this.handleUpdateTodo}
